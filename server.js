4029967fcd42c69df2600fd8b2e2d93ec0bc4f68
Ridoy Majumdar <majumdar.ridoy@gmail.com>
2016-07-25 18:37:54 -0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,10 @@
+var io      = require('socketio'),
+    app     = require('express')(),
+    uuid    = require('node-uuid'),
+    http    = require('http');
+
+app.get('/', function(req,res) {
+
+})
+
+app.listen(4000);
